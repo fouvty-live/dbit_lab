3dc6b6a8d7fe5ce8ab228ed4754171acfe85d554
fouvty <fouvtylive@gmail.com>
2025-02-16 16:07:34 +0530

    [updt]-Sale order filter based on B2C & B2B

--- dbitlab_base/__manifest__.py
+++ dbitlab_base/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': """""",
     'category': "",
     'author': "",
-    'depends': ['stock', 'product'],
+    'depends': ['stock', 'product','contacts', 'sale'],
     'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
@@ -12,7 +12,8 @@
         'views/prodcuct_group_view.xml',
         'views/product_brand.view.xml',
         'views/route_customer_view.xml',
-        'views/res_partner_view.xml'
+        'views/res_partner_view.xml',
+        'views/sale_order_views.xml',
     ],
     'installable': True,
     'auto_install': False,
--- dbitlab_base/models/res_partner.py
+++ dbitlab_base/models/res_partner.py
@@ -12,3 +12,7 @@ class ResPartner(models.Model):
     customer_class = fields.Char(string="Customer Class")
     ledger_id = fields.Integer(string="Ledger ID")
     route_id = fields.Many2one('route.customer', string="Route")
+    customer_type = fields.Selection([
+        ('b2b', 'Business-to-Business (B2B)'),
+        ('b2c', 'Business-to-Clients (B2C)')
+    ], string="Customer Type", required=True, default='b2c')
